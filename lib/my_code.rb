8f0d39e886b7b53c160ef869a436604395714d68
vien pham <vienpham2019@gmail.com>
2019-12-12 21:42:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.map{|n| n * -1}
+end
+
+def map_to_no_change(source_array)
+  source_array.map{|n| n}
+end
+
+def map_to_double(source_array)
+  source_array.map{|n| n * 2}
+end
+
+def map_to_square(source_array)
+  source_array.map{|n| n ** 2}
+end
+
+def reduce_to_total(source_array,starting_point = 0)
+  source_array.reduce(starting_point){|sum, n| sum + n}
+end
+
+def reduce_to_all_true(source_array)
+  source_array.map {|e| return !!e if !!e == false}
+end
+
+def reduce_to_any_true(source_array)
+  source_array.map {|e| return true if !! e == true}
+  return false
+end
+
