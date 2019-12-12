37bb495ad8be951ff09ae36587c985288ee93840
vien pham <vienpham2019@gmail.com>
2019-12-12 20:00:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce_to_total(source_array,starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce {|element| break false if element != true}
+  source_array.reduce {|element| break true if element != true}
 end
 
 def reduce_to_any_true(source_array)
