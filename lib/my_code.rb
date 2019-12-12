c792eea1a3e3358e1ad5572980c62df9d223a1b0
vien pham <vienpham2019@gmail.com>
2019-12-12 21:18:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,6 +24,6 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  source_array.map {|e| return !!e if !!e == true}
+  source_array.map {|e| if !!e == false||!!e == nil; return false;else return true end}
 end
 
